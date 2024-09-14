1077a05fa5bea7d78ffc3c6a2340db37a5da7196
Yun Seok Yang <yunseok.yang@mail.utoronto.ca>
2024-09-14 12:46:48 -0400

    camera move script + camera prefab

---
+++ PukingPredator/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    void Start()
+    {
+        if (player == null) player = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 15, -15);
+    }
+}
