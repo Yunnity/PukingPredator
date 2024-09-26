2d9c74f29403b3e23bb5f2c3c7df4bd4d4485299
crookedspider1 <williamlahaye@hotmail.com>
2024-09-26 02:29:42 -0400

    WIP started refactoring the player script

--- PukingPredator/Assets/Scripts/Player.cs
+++ PukingPredator/Assets/Scripts/Player.cs
@@ -1,55 +1,108 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private GameInput gameInput;
-    [SerializeField] private Inventory inventory;
-    [SerializeField] private LayerMask consumable;
-    [SerializeField] private GameObject playerCamera;
+    /// <summary>
+    /// The mass of the player before having eaten anything.
+    /// </summary>
+    private float baseMass;
 
-    private Rigidbody rb;
+    /// <summary>
+    /// The base move speed before added weight is applied. (TEMP)
+    /// </summary>
     private float baseMoveSpeed = 10f;
-    private float moveSpeed;
 
+    /// <summary>
+    /// The layers that consumable objects can be on. (TEMP???)
+    /// </summary>
+    [SerializeField]
+    private LayerMask consumableLayers;
+
+    /// <summary>
+    /// The handler for user input.
+    /// </summary>
+    [SerializeField]
+    private GameInput gameInput;
+
+    /// <summary>
+    /// The players inventory.
+    /// </summary>
+    [SerializeField]
+    private Inventory inventory;
+
+    /// <summary>
+    /// The direction of the players recent movement input with the camera rotation applied.
+    /// </summary>
     private Vector3 moveDir;
 
+    /// <summary>
+    /// The speed that the player moves in any given direction.
+    /// </summary>
+    private float moveSpeed;
+
+    /// <summary>
+    /// A reference to the camera, used for correcting movement direction.
+    /// </summary>
+    [SerializeField]
+    private GameObject playerCamera;
+
+    /// <summary>
+    /// The distance that items spawn ahead of the player when puking. (TEMP)
+    /// </summary>
     private float pukeDistance = 2f;
+
+    /// <summary>
+    /// The rigidbody of the player.
+    /// </summary>
+    private Rigidbody rb;
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        baseMass = rb.mass;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDir * moveSpeed + new Vector3(0, rb.velocity.y, 0);
+    }
 
     private void Start()
     {
-        gameInput.onEat += GameInput_OnEat;
-        gameInput.onPuke += GameInput_OnPuke;
+        gameInput.onEat += GameInput_Eat;
+        gameInput.onPuke += GameInput_Puke;
         gameInput.onResetLevel += GameInput_ResetLevel;
 
         inventory.onChange += UpdateMovementAttributes;
         UpdateMovementAttributes();
 
-        rb = GetComponent<Rigidbody>();
         if (playerCamera == null) { playerCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0]; }
     }
 
-    private void GameInput_OnPuke()
+    void Update()
     {
-        if (inventory.isEmpty) { return; }
+        Vector2 inputVector = gameInput.movementInput;
+        moveDir = Quaternion.Euler(0, playerCamera.transform.eulerAngles.y, 0) * new Vector3(inputVector.x, 0, inputVector.y);
 
-        Consumable itemToPlace = inventory.PopItem();
-
-        var pukeDir = transform.forward;
-        itemToPlace.PlaceAt(transform.position + pukeDir*pukeDistance);
+        float turnSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * turnSpeed);
     }
 
-    private void GameInput_OnEat()
+
+
+    private void GameInput_Eat()
     {
         if (inventory.isFull) { return; }
 
         // Define the ray, starting from the player's position, shooting forward
-        Ray ray = new Ray(transform.position, transform.forward);
+        var ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-        float sphereRadius = 0.5f; // Adjust as needed
-
-        // Perform the sphere cast
-        if (Physics.SphereCast(ray, sphereRadius, out hit, 5f, consumable)) // 5f is the maximum ray distance
+        if (Physics.SphereCast(ray, radius: 0.5f, out hit, maxDistance: 5f, layerMask: consumableLayers))
         {
             // Get the GameObject that was hit
             GameObject hitObject = hit.collider.gameObject;
@@ -62,27 +115,20 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void GameInput_Puke()
+    {
+        if (inventory.isEmpty) { return; }
+
+        Consumable itemToPlace = inventory.PopItem();
+
+        var pukeDir = transform.forward;
+        itemToPlace.PlaceAt(transform.position + pukeDir * pukeDistance);
+    }
+
     public void GameInput_ResetLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 inputVector = gameInput.movementInput;
-        moveDir = Quaternion.Euler(0, playerCamera.transform.eulerAngles.y, 0) * new Vector3(inputVector.x, 0, inputVector.y);
-
-        float turnSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * turnSpeed);
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = moveDir * moveSpeed + new Vector3(0, rb.velocity.y, 0);
-    }
-
-
 
     private void UpdateMovementAttributes()
     {
