95e67a615d2ba0286540bd5e4729f2c69a010127
crookedspider1 <williamlahaye@hotmail.com>
2024-10-14 19:05:22 -0400

    Updated main menu to use game manager, added setup stuff to the main menu

--- PukingPredator/Assets/Scripts/Menus/MainMenu.cs
+++ PukingPredator/Assets/Scripts/Menus/MainMenu.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void OnPlayButtonClicked()
     {
-        SceneManager.LoadScene("PlayableLevel");
+        GameManager.TransitionToScene("PlayableLevel");
     }
 
     public void OnQuitButtonClicked()
