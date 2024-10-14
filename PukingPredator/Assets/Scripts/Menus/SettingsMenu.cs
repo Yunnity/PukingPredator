3900b1897f5dc9ee37dcc35b8fbb66329544900e
Chan Yang <yunseok.yang@mail.utoronto.ca>
2024-10-14 15:00:20 -0400

    title screen

---
+++ PukingPredator/Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject keyBoardControls;
+    [SerializeField] private GameObject controllerControls;
+    public void OnNextButtonClicked()
+    {
+        if (keyBoardControls.activeInHierarchy)
+        {
+            keyBoardControls.SetActive(false);
+            controllerControls.SetActive(true);
+        }
+        else
+        {
+            keyBoardControls.SetActive(true);
+            controllerControls.SetActive(false);
+        }
+    }
+}
