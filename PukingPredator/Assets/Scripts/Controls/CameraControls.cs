262a33ce51af041bcf7a70e20c20da3494ac65f3
crookedspider1 <williamlahaye@hotmail.com>
2024-10-15 22:13:44 -0400

    Made CameraControls an InputBehaviour and added gamepad input

--- PukingPredator/Assets/Scripts/Controls/CameraControls.cs
+++ PukingPredator/Assets/Scripts/Controls/CameraControls.cs
@@ -1,38 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 
-public class CameraControls : MonoBehaviour
+public class CameraControls : InputBehaviour
 {
+    /// <summary>
+    /// Highest Y value for the rotation. This is how far down the camera can go
+    /// below the player.
+    /// </summary>
+    private float maxY = 34;
 
-    public Vector2 turn;
-    private float sens = 6f;
-    private float maxY = 40;
+    /// <summary>
+    /// Lowest Y value for the rotation. This is how far up the camera can go
+    /// above the player.
+    /// </summary>
     private float minY = -40;
-    public float speed = 1;
-    public Transform _transform;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// The current camera rotation.
+    /// </summary>
+    private Vector2 rotation;
+
+    /// <summary>
+    /// The sensitivity of the camera movement.
+    /// </summary>
+    private float sensitivity = 1f;
+
+
+
     void Start()
     {
+        //lock the mouse to the center of the view
         Cursor.lockState = CursorLockMode.Locked;
-        _transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        turn.x += Input.GetAxis("Mouse X") * sens;
-        turn.y += Input.GetAxis("Mouse Y") * sens;
-        if (turn.y < minY)
-        {
-            turn.y = minY;
-        }
-        else if (turn.y > maxY)
-        {
-            turn.y = maxY;
-        }
-        _transform.rotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        rotation += gameInput.cameraInput * sensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, minY, maxY);
+
+        transform.rotation = Quaternion.Euler(-rotation.y, rotation.x, 0);
     }
 }
