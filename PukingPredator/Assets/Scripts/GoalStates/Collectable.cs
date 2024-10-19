c7f46ffe021955c8ee8637e33c7a7b64910d83ca
crookedspider1 <williamlahaye@hotmail.com>
2024-10-19 11:44:17 -0400

    Refactored "Objective" into "Collectable"

---
+++ PukingPredator/Assets/Scripts/GoalStates/Collectable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public CollectableTracker tracker;
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            tracker.CollectedOne();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PukingPredator/Assets/Scripts/GoalStates/CollectableTracker.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableTracker : MonoBehaviour
+{
+    /// <summary>
+    /// The main UI component for the collectable data.
+    /// </summary>
+    private GameObject collectableUI;
+
+    /// <summary>
+    /// TEMP: The text box used for showing how many collectables are left.
+    /// </summary>
+    private Text collectablesLeftText;
+
+    /// <summary>
+    /// The number of collectables in the level still.
+    /// </summary>
+    private int remainingCollectables;
+
+
+
+    void Start()
+    {
+        collectableUI = GameObject.Find("objectsLeftText");
+        collectablesLeftText = collectableUI.GetComponent<Text>();
+
+        // find all collectables
+        var collectables = FindObjectsOfType<Collectable>();
+        remainingCollectables = collectables.Length;
+        foreach (var collectable in collectables)
+        {
+            collectable.tracker = this;
+        }
+
+        UpdateUI();
+    }
+
+
+
+    /// <summary>
+    /// Called when a collectable has been collected.
+    /// </summary>
+    public void CollectedOne()
+    {
+        remainingCollectables--;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (remainingCollectables > 0)
+        {
+            collectablesLeftText.text = $"Remaining Collectables: {remainingCollectables}";
+        }
+        else
+        {
+            collectablesLeftText.text = "Collected Everything";
+        }
+    }
+
+}
