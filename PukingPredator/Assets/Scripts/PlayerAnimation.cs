635dfed8213af781f9c5af5cd88f2aa696808ae6
dungwoong <78326933+dungwoong@users.noreply.github.com>
2024-10-20 18:13:03 -0400

    add animation, state script for moving

---
+++ PukingPredator/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private const float MOVING_THRESHOLD = 0.001f;
+
+    [SerializeField]
+    private GameObject model;
+
+    private Animator animator;
+    private Rigidbody rb;
+
+    int isMovingHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = model.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        isMovingHash = Animator.StringToHash("isMoving");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateStates();
+    }
+
+    // Check animation state
+    // Currently, we only have moving and idle
+    void updateStates()
+    {
+        bool horizontalMovement = Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.z) > MOVING_THRESHOLD;
+        animator.SetBool(isMovingHash, horizontalMovement);
+    }
+}
