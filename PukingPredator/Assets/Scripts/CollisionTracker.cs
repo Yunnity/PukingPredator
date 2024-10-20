7cc8b65fc434bb50a5f113088bbc867cdbe04787
crookedspider1 <williamlahaye@hotmail.com>
2024-10-19 22:48:32 -0400

    Added component for tracking what objects are currently colliding

---
+++ PukingPredator/Assets/Scripts/CollisionTracker.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CollisionTracker : MonoBehaviour
+{
+    private List<GameObject> _collisions = new();
+    /// <summary>
+    /// The list of things currently colliding with the object.
+    /// </summary>
+    public List<GameObject> collisions => new(_collisions);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_collisions.Contains(other.gameObject))
+        {
+            _collisions.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _collisions.Remove(other.gameObject);
+    }
+
+    private void Update()
+    {
+        // Remove destroyed objects from the list
+        _collisions = _collisions.Where(c => c != null).ToList();
+    }
+}
