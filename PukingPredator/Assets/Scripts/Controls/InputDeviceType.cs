001dbdfbd31ccdbd3f9f3e84cd2c749df04a6b52
crookedspider1 <williamlahaye@hotmail.com>
2024-11-05 20:24:23 -0500

    Added input device types and control swap detection

---
+++ PukingPredator/Assets/Scripts/Controls/InputDeviceType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public enum InputDeviceType
+{
+    keyboard,
+    mouse,
+    gamepad,
+    other,
+}
+
+static class InputDeviceTypeMethods
+{
+    public static bool IsKeyboardOrMouse(this InputDeviceType idt)
+    {
+        switch (idt)
+        {
+            case InputDeviceType.keyboard:
+                return true;
+            case InputDeviceType.mouse:
+                return true;
+            default:
+                return false;
+        }
+    }
+}
