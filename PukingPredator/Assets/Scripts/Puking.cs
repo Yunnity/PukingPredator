b6f3071a8b3f024b3fabc8b400114432609ca741
dungwoong <78326933+dungwoong@users.noreply.github.com>
2024-11-08 09:54:30 -0500

    Add explosion for puking

--- PukingPredator/Assets/Scripts/Puking.cs
+++ PukingPredator/Assets/Scripts/Puking.cs
@@ -24,6 +24,16 @@ public class Puking : InputBehaviour
     private Vector3 pukeDirection => transform.forward + Vector3.up * 0.1f;
 
     /// <summary>
+    /// If pukeForce is greater than this, the puke will also knock back items in front of the player
+    /// </summary>
+    private const float PUKE_EXPLODE_THRESH = 0.8f;
+
+    /// <summary>
+    /// If you have this many items in the inventory and you puke with force, you will knock back items in front of you
+    /// </summary>
+    private const int KNOCKBACK_ITEM_THRESH = 5;
+
+    /// <summary>
     /// Force applied to object when puked. Depends on how long the puke button
     /// was held down for.
     /// </summary>
@@ -75,8 +85,38 @@ public class Puking : InputBehaviour
             //...the velocity and if there was a rigid body then we can reduce the velocity
             //...while calculating it based on the mass?
             itemRb.AddForce(pukeVelocity * itemRb.mass, ForceMode.Impulse);
+
+            // -1 because player loses an item when they puke
+            if (pukeForce > MAX_PUKE_FORCE * PUKE_EXPLODE_THRESH && inventory.itemCount >= KNOCKBACK_ITEM_THRESH - 1)
+            {
+                KnockbackItemsInFrontofPlayer(pukeVelocity * pukeForce, pukeForce, itemRb);
+            }
         }
 
+    }
+
+    public void KnockbackItemsInFrontofPlayer(Vector3 vec, float f, Rigidbody itemRB)
+    {
+
+        Vector3 halfExtents = new Vector3(1f, 0.5f, 0.5f); // Half the size of the box (x, y, z)
+
+        // BoxCast in the direction of the velocity
+        RaycastHit[] hits = Physics.BoxCastAll(transform.position + (transform.forward * 0.1f) + (transform.up * 0.6f), halfExtents, vec, Quaternion.identity, 3f);
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (!hit.collider.CompareTag(GameTag.player))
+            {
+                Rigidbody hitRB = hit.collider.GetComponent<Rigidbody>();
+                PhysicsBehaviour pb = hit.collider.GetComponent<PhysicsBehaviour>();
+                if (hitRB != null && pb != null && hitRB != itemRB)
+                {
+                    pb.EnablePhysics();
+                    hitRB.AddExplosionForce(1f * f, transform.position, 5f, 0.01f, ForceMode.VelocityChange);
+                    //hitRB.AddForce(vec * hitRB.mass * 0.5f, ForceMode.Impulse); // alternate force if you want to push things forward
+                }
+            }
+        }
     }
 
 }
