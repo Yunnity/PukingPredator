17793a507065b7a9783480d1cf2b75ec93d03cb7
crookedspider1 <williamlahaye@hotmail.com>
2024-11-08 00:37:22 -0500

    Added material and component for animated dashed lines

---
+++ PukingPredator/Assets/Scripts/UI/DashedLineAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class DashedLineAnimation : MonoBehaviour
+{
+    public float scrollSpeed = 2.0f;
+    private Material lineMaterial;
+
+    void Start()
+    {
+        lineMaterial = GetComponent<LineRenderer>().material;
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed * -1;
+        lineMaterial.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
