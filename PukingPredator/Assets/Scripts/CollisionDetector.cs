803420cd5c7f034ccbf93b571c14cbff3e4ad8aa
crookedspider1 <williamlahaye@hotmail.com>
2024-11-13 14:39:35 -0500

    Added a component for triggering events on an objects collisions

---
+++ PukingPredator/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    private Action onCollision;
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onCollision?.Invoke();
+    }
+
+
+
+    public void Subscribe(Action action)
+    {
+        onCollision += action;
+    }
+
+    public void Unsubscribe(Action action)
+    {
+        onCollision -= action;
+    }
+}
+
