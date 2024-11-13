91d60afc0034e5eebd00dd239f92cc6f100abca5
crookedspider1 <williamlahaye@hotmail.com>
2024-11-13 13:40:44 -0500

    Made FollowObject code

---
+++ PukingPredator/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    /// <summary>
+    /// If the object should copy changes to the players scale.
+    /// </summary>
+    [SerializeField]
+    private float copyTargetScaleFactor = 0;
+
+    /// <summary>
+    /// The initial scale of this object.
+    /// </summary>
+    private Vector3 initialScale;
+
+    /// <summary>
+    /// The initial scale of the player object.
+    /// </summary>
+    private float initialTargetScaleMagnitude;
+
+    /// <summary>
+    /// The relative position to the player.
+    /// </summary>
+    private Vector3 offset;
+
+    /// <summary>
+    /// Player object to follow
+    /// </summary>
+    [SerializeField]
+    public GameObject target;
+
+
+
+    protected virtual void Start()
+    {
+        offset = transform.position - target.transform.position;
+
+        initialTargetScaleMagnitude = target.transform.localScale.magnitude;
+        initialScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        var relativeTargetScale = target.transform.localScale.magnitude / initialTargetScaleMagnitude;
+        //copy "copyTargetScaleFactor" percent of the scale CHANGE
+        var multiplier = 1 + (relativeTargetScale - 1) * copyTargetScaleFactor;
+
+        transform.position = target.transform.position + offset * multiplier;
+
+        transform.localScale = initialScale * multiplier;
+    }
+}
+
--- PukingPredator/Assets/Scripts/FollowPlayer.cs
+++ PukingPredator/Assets/Scripts/FollowPlayer.cs
@@ -1,54 +1,14 @@
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class FollowPlayer : FollowObject
 {
-    /// <summary>
-    /// If the object should copy changes to the players scale.
-    /// </summary>
-    [SerializeField]
-    private float copyPlayerScaleFactor = 0;
-
-    /// <summary>
-    /// The initial scale of the player object.
-    /// </summary>
-    private float initialPlayerScaleMagnitude;
-
-    /// <summary>
-    /// The initial scale of this object.
-    /// </summary>
-    private Vector3 initialScale;
-
-    /// <summary>
-    /// The relative position to the player.
-    /// </summary>
-    private Vector3 offset;
-
-    /// <summary>
-    /// Player object to follow
-    /// </summary>
-    [SerializeField]
-    private GameObject player;
-
-    
-
-    void Start()
+    protected override void Start()
     {
-        player ??= GameObject.FindGameObjectsWithTag("Player")[0];
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectsWithTag("Player")[0];
+        }
 
-        offset = transform.position - player.transform.position;
-
-        initialPlayerScaleMagnitude = player.transform.localScale.magnitude;
-        initialScale = transform.localScale;
-    }
-
-    void Update()
-    {
-        var relativePlayerScale = player.transform.localScale.magnitude / initialPlayerScaleMagnitude;
-        //copy "copyPlayerScaleFactor" percent of the scale CHANGE
-        var multiplier = 1 + (relativePlayerScale - 1) * copyPlayerScaleFactor;
-
-        transform.position = player.transform.position + offset * multiplier;
-
-        transform.localScale = initialScale * multiplier;
+        base.Start();
     }
 }
