a93d17c9e130d1a8d81b5dff56ff2bdea1d4d9a0
crookedspider1 <williamlahaye@hotmail.com>
2024-11-13 14:12:19 -0500

    Created a component to destroy an object if an RBs velocity is low

---
+++ PukingPredator/Assets/Scripts/DestroyOnLowVelocity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DestroyOnLowVelocity : MonoBehaviour
+{
+    /// <summary>
+    /// The minimum velocity magnitude before it gets destroyed before.
+    /// </summary>
+    [SerializeField]
+    private float minVelocity;
+
+    /// <summary>
+    /// The rigidbody to get the velocity from.
+    /// </summary>
+    public Rigidbody rb;
+
+    /// <summary>
+    /// Used to skip the first frame.
+    /// </summary>
+    private bool skipFrame = true;
+
+
+
+    void Update()
+	{
+        if (skipFrame)
+        {
+            skipFrame = false;
+            return;
+        }
+
+        if (rb.velocity.magnitude <= minVelocity) { Destroy(gameObject);  }
+	}
+}
+
