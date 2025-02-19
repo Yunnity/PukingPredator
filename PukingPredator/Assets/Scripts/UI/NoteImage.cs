aab87c623522ad85672502bcfdb43d278e95c721
crookedspider1 <williamlahaye@hotmail.com>
2025-02-19 13:34:11 -0500

    Updated note base class to use a container and handle more
    The note base class can now handle multiple UI components being added to a note (in preparation for text + image + text for controls being displayed).
    The text and image classes that inherit from it are now very simple, they just request to have the text or image made and do nothing else.

--- PukingPredator/Assets/Scripts/UI/NoteImage.cs
+++ PukingPredator/Assets/Scripts/UI/NoteImage.cs
@@ -1,13 +1,7 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class NoteImage : Note
 {
-    /// <summary>
-    /// To store the original alpha of the image.
-    /// </summary>
-    private float originalAlpha;
-
     /// <summary>
     /// The image shown by the note.
     /// </summary>
@@ -15,52 +9,16 @@ public class NoteImage : Note
     private Sprite sprite;
 
     /// <summary>
-    /// A reference to the image this note is for.
+    /// Max sizes of the image.
     /// </summary>
-    private Image UIImage;
+    [SerializeField]
+    private Vector2 size = new Vector2(36f, 36f);
 
 
 
-    private void Awake()
+    protected override void Awake()
     {
-        CreateImageComponent();
-    }
-
-
-
-    /// <summary>
-    /// Create the Image component dynamically.
-    /// </summary>
-    void CreateImageComponent()
-    {
-        var canvas = GameObject.Find("Canvas");
-
-        // Create a new GameObject and make it a child of the Canvas
-        GameObject imageObject = new GameObject("ProximityImage");
-        imageObject.transform.SetParent(canvas.transform, false); // Ensure it stays relative to the canvas
-
-        // Add an Image component to the GameObject
-        UIImage = imageObject.AddComponent<Image>();
-        UIImage.sprite = sprite;
-
-        RectTransform rectTransform = UIImage.GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = new Vector2(0, 0);
-        //TODO: pull this out into a property? fix it so it scales correctly?
-        //TODO: should this be done with sizeDelta? thats what i saw online, but it didnt work
-        rectTransform.localScale = Vector3.one * 6f;
-
-        // Store the original alpha of the image
-        originalAlpha = UIImage.color.a;
-    }
-
-    /// <summary>
-    /// Helper function to set the alpha of the UI Image.
-    /// </summary>
-    /// <param name="alpha"></param>
-    protected override void SetAlpha(float alpha)
-    {
-        Color color = UIImage.color;
-        color.a = alpha * originalAlpha;
-        UIImage.color = color;
+        base.Awake();
+        _ = AddImage(sprite, size);
     }
 }
