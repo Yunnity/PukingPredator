1926e885d0359f67b8898ceb0d4151e45c9384f8
MD-Hamza <71731160+MD-Hamza@users.noreply.github.com>
2025-03-12 03:33:59 -0400

    Disabled movement after eating

--- PukingPredator/Assets/Scripts/MovableItem.cs
+++ PukingPredator/Assets/Scripts/MovableItem.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MovableItem : PhysicsCollapseGroup
 {
+    protected override void Start()
+    {
+        base.Start();
 
+        foreach (var child in relevantChildren)
+        {
+            child.Subscribe(PhysicsEvent.onEnable, disableMovement);
+        }
+    }
+
+    private void disableMovement()
+    {
+        enabled = false;
+    }
 }
